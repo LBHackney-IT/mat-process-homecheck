39465403fe79698abc7931f21dcc12ed4ffeb318
LBHABEBIC <ana.bebic@hackney.gov.uk>
2020-08-10 16:47:52 +0100

    remove signature and fix next page in other support

--- steps/id-and-residency/other-support.ts
+++ steps/id-and-residency/other-support.ts
@@ -49,7 +49,7 @@ const step: ProcessStepDefinition<ResidentDatabaseSchema, "otherSupport"> = {
   },
   step: {
     slug: PageSlugs.OtherSupport,
-    nextSlug: PageSlugs.Verify,
+    nextSlug: PageSlugs.Sections,
     submit: (nextSlug?: string): ReturnType<typeof makeSubmit> =>
       makeSubmit({
         slug: nextSlug as PageSlugs | undefined,
